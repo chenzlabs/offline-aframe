612c3ed02214b5cf35e1f3e160ad2e29e00fe702
machenmusik <machenmusik@gmail.com>
2017-07-10 13:22:38 -0400

    since font PNG textures are now inlined, don't preload files

--- src/preload-fonts.js
+++ src/preload-fonts.js
@@ -1,22 +1,23 @@
 // Include the fonts the hard way (have to enumerate all of them)
+// Note that PNG files are now inlined, so excluded here
 
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Roboto-msdf.json');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Roboto-msdf.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Roboto-msdf.png');
 
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Aileron-Semibold.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Aileron-Semibold.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Aileron-Semibold.png');
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/DejaVu-sdf.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/DejaVu-sdf.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/DejaVu-sdf.png');
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Exo2Bold.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Exo2Bold.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Exo2Bold.png');
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Exo2SemiBold.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Exo2SemiBold.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Exo2SemiBold.png');
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/KelsonSans.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/KelsonSans.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/KelsonSans.png');
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Monoid.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Monoid.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/Monoid.png');
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/mozillavr.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/mozillavr.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/mozillavr.png');
 require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/SourceCodePro.fnt');
-require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/SourceCodePro.png');
+// require('file-loader?name=fonts/[name].[ext]!../node_modules/aframe-assets/fonts/SourceCodePro.png');
 
